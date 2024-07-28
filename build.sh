666af4ab34b6d7d0f4ec75eade5ffd51359715c9
Ahilan-1 <166643931+Ahilan-1@users.noreply.github.com>
2024-07-28 19:20:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-# build.sh
 #!/bin/bash
-pip install --upgrade pip==22.0.4
+python -m pip install --upgrade pip==22.0.4
+pip install -r requirements.txt
+python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords')"
